faa40db14a860fef80baf9cca177bc1ddfbef7b8
mjpark <mjpark@korbit.co.kr>
2015-03-19 20:08:23 +0900

    Web Framework Init

---
+++ app.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+// 
+var http = require('http');
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var app = express();
+
+//
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+//
+http.createServer(app).listen(3000, function(){
+	console.log('App started');
+});
+
+//
+app.get('/', function(req, res){
+	res.render('index');
+});
