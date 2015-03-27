169c7d894f03e43893bafb5ca4a82f0a550e2933
Chungha Yun <chungha@chungha-macbookair.roam.corp.google.com>
2015-03-27 19:37:18 +0900

    test pass for source_mock

--- models/source_mock.js
+++ models/source_mock.js
@@ -1,14 +1,46 @@
+HashMap = require('hashmap').HashMap
+
 module.exports = function () {
   return new SourceTable();
 }
 
+var sampleSource = {
+  file:'sample',
+  source:'object HelloWorld { def main(args: Array[String]) { println(\"Hello, world!\") } }',
+  entities:['Array','String','println']
+};
+
+var arrayEntity = {
+  url:'http://www.scala-lang.org/api/current/#scala.Array'
+};
+var stringEntity = {
+  url:'http://www.scala-lang.org/api/current/#scala.Predef$'
+};
+var printlnEntity = {
+  url:'http://www.scala-lang.org/api/current/#scala.Console$'
+};
+
 function SourceTable() {
+  this.sourceMap = new HashMap();
+  this.sourceMap.set("sample", sampleSource);
+  this.entityMap = new HashMap();
+  this.entityMap.set("Array", arrayEntity);
+  this.entityMap.set("String", stringEntity);
+  this.entityMap.set("println", printlnEntity);
 }
 
 SourceTable.prototype.hasSource = function(name) {
-  return false;
+  return this.sourceMap.has(name);
 }
 
 SourceTable.prototype.getSource = function(name) {
-  return '';
+  return JSON.stringify(this.sourceMap.get(name));
+}
+
+SourceTable.prototype.getSourceJson = function(name) {
+  return this.sourceMap.get(name);
+}
+
+SourceTable.prototype.getEntityJson = function(entity) {
+  return this.entityMap.get(entity);
 }
--- models/test/source_mock_test.js
+++ models/test/source_mock_test.js
@@ -4,8 +4,33 @@ var source = require('../source_mock');
 
 var mock = source();
 
+var sampleSource = "{\"file\":\"sample\",\"source\":\"object HelloWorld { def main(args: Array[String]) { println(\\\"Hello, world!\\\") } }\",\"entities\":[\"Array\",\"String\",\"println\"]}";
+
 describe('Test source mock', function() {
   it ('dont have source', function() {
     test.assert(!mock.hasSource('aaa'));
+  });
+  it ('have source', function() {
+    test.assert(mock.hasSource('sample'));
+  });
+  it ('get source', function() {
+    test.string(mock.getSource('sample')).is(sampleSource);
+  });
+  it ('get source json object', function() {
+    var json = mock.getSourceJson('sample');
+    test.string(JSON.stringify(json)).is(sampleSource);
+  });
+  it ('get entity', function() {
+    var json = mock.getSourceJson('sample');
+    var entities = json['entities'];
+    test.string(entities[0]).is('Array');
+    test.string(entities[1]).is('String');
+    test.string(entities[2]).is('println');
+  });
+  it ('get entity url', function() {
+    var json = mock.getSourceJson('sample');
+    var entities = json['entities'];
+    var arrayEntityJson = mock.getEntityJson(entities[0]);
+    test.string(arrayEntityJson['url']).is('http://www.scala-lang.org/api/current/#scala.Array');
   });
 });
