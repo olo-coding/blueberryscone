7027ea8b00d7cb56b38197787c2b3d4a36b79fc9
mjpark <mjpark@korbit.co.kr>
2015-04-02 03:24:42 +0900

    1) add API to get source & entities, 2) add modules in source_mock.js, 3) add test in source_mock_test.js

--- controllers/test.js
+++ controllers/test.js
@@ -1,8 +1,40 @@
-
+/*jslint node: true */
+/*jslint nomen: true */
 'use strict';
 
-var  models;
+var models;
 
+var source = require('../models/source_mock');
+var mock = source();
+
+/*
+	get source & entities
+
+	by Rachel
+*/
+var getSource = function(req, res) {
+
+	var result = {}, entitiyMap, entityArray = [];
+
+	// 1. search source
+
+	// 2. create JSON object
+	result.source = mock.getSource('sample');
+
+	entitiyMap = mock.getEntityMap();
+	entitiyMap.forEach(function(value, key) {
+    	entityArray.push({name: key, url: value.url});
+	});
+	result.entity = entityArray;
+
+	// 3. return JSON
+	res.contentType('application/json');
+	res.send(JSON.stringify(result));
+};
+
+/*
+	global module
+*/
 module.exports = function(app, bodyParser, dbModels){
 
 	models = dbModels;
@@ -15,10 +47,12 @@ module.exports = function(app, bodyParser, dbModels){
 		models.Test.find({
 				where: {id: '1'}
 			}).complete(function(err, testObj){
-
 				console.log('TEST data: ' + testObj.title);
 			});
 
 		res.send('TEST DB select success!');
 	});
+
+	// 
+	app.get('/source', getSource);
 };
